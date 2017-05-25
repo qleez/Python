b355374cadb610e82a93d8d2aefc919680c8192e
Qian Shao <1056829819@qq.com>
2017-05-25 11:28:14 +0800

    Update reverse_complement.py

--- reverse_complement.py
+++ reverse_complement.py
@@ -4,6 +4,13 @@
 # Return: The reverse complement sc of s.
 # http://rosalind.info/problems/revc/
 
+'''
+"ATTCAG"[::-1]取"ATTCAG"的反向串"GACTTA"
+在"TAGC"中寻找G、A、C、T、T、A出现的位置，2、1、3、0、0、1
+找到"ATCG"[2]、"ATCG"[1]、"ATCG"[3]、"ATCG"[0]、"ATCG"[0]、"ATCG"[1]
+并把找出的C、T、G、A、A、T几个字母join在一起，构成CTGAAT
+'''
+
 print(''.join(["ATCG"["TAGC".index(n)]
                for n in "ATTCAG"[::-1]
               ]
