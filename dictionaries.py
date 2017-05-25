0bcd8701fc20739d5066d4c6b9cf6bfae8ec78f7
Qian Shao <1056829819@qq.com>
2017-05-25 10:22:17 +0800

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,9 @@
 #coding=utf-8
 
 import collections
-import os
 
-with open('str.txt') as file1:#打开文本文件
-    str1=file1.read().split(' ')#将文章按照空格划分开 
+str = ''
+str1 = str.split(' ')  #将字符串按照空格划分开
 
-print "原文本:\n %s"% str1
-print "\n各单词出现的次数：\n %s" % collections.Counter(str1)
+for key, value in collections.Counter(str1).items():
+    print(key + ' ' + value)
