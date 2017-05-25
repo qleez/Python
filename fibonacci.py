90d2b77fbd137a76bb112837a7ee407d7ae17bee
Qian Shao <1056829819@qq.com>
2017-05-25 14:11:07 +0800

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -8,6 +8,6 @@ def fib(n, k):
     a, b = 1, 1
     for i in range(2, int(n)):
         # 只需要保存最近两个月的数量即可
-        a, b = b, int(k)  *a + b
+        a, b = b, int(k) * a + b
     print(b)
 fib(5, 3)
