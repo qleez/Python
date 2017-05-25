dbd2c11b8b8ceea96b921fe1ee733ac7bb6e2498
Qian Shao <1056829819@qq.com>
2017-05-25 10:53:37 +0800

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,9 +1,14 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
+
+# Given: A string s of length at most 10000 letters.
+# Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order.
+# http://rosalind.info/problems/ini6/
 
 import collections
 
 str = ''
-str1 = str.split(' ')  #将字符串按照空格划分开
+# 将字符串按照空格划分开
+str1 = str.split(' ')
 
 for key, value in collections.Counter(str1).items():
     print('%s %s' %(key, value))
